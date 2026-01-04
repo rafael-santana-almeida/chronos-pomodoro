da002671cb3d863bc2f5fe16f78bc00b3ab5df83
Rafael Santana <rafaelsantana.devweb@gmail.com>
2026-01-04 12:06:42 -0300

    Create Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 import './styles/global.css';
 import './styles/theme.css';
 
@@ -6,7 +7,7 @@ export function App() {
   return (
     <>
       <Container>
-        <section>LOGO</section>
+        <Logo />
       </Container>
     </>
   );
